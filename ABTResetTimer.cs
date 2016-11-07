744e7ecf6af19b5b758eac21e19d728d6087377a
SfTd <j.zolcik@allblue.pl>
2016-11-07 09:14:06 +0100

    Added `ABTResetTimer`.

---
+++ ABTResetTimer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ABTouch
+{
+    public class ABTRestTimer
+    {
+
+        const int DefaultTimerInterval = 5000;
+
+
+        private Window window = null;
+        private Action callback = null;
+
+        private DispatcherTimer timer = null;
+
+        int timerInterval = 5;
+
+
+        public ABTRestTimer(Window window, Action callback, int timer_interval_in_seconds)
+        {
+            this.window = window;
+            this.callback = callback;
+            this.timerInterval = timer_interval_in_seconds;
+
+            window.AddHandler(Button.ClickEvent, new RoutedEventHandler(this.button_Click), true);
+            window.AddHandler(UIElement.MouseUpEvent, new MouseButtonEventHandler(this.uiElement_MouseButtonUp), true);
+
+            this.resetTimer();
+        }
+
+        public ABTRestTimer(Window window, Action callback) :
+                this(window, callback, ABTRestTimer.DefaultTimerInterval)
+        {
+
+        }
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.resetTimer();
+        }
+
+        private void resetTimer()
+        {
+            if (this.timer != null)
+                this.timer.Stop();
+
+            this.timer = new DispatcherTimer();
+            this.timer.Tick += this.timer_Tick;
+            this.timer.Interval = new TimeSpan(0, 0, this.timerInterval);
+            this.timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this.callback();
+        }
+
+        private void uiElement_MouseButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.resetTimer();
+        }
+
+    }
+}
+
